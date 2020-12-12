848fce65b56f42e6de193216f58e3f16a8e37110
Bjoern Aust <bjoern.aust@lnix.de>
2020-12-12 18:14:59 +0100

    day 12

--- day_12.py
+++ day_12.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python
-#import numpy as np
-#import matplotlib.pyplot as plt
-import sys
 
 # day 12: Rain Risk
 
@@ -12,32 +9,34 @@ lines = fobj.readlines()
 for line in lines:
     line = line.replace('\n','')
     input_list.append((line[0],line[1::]))
-
 #print input_list
-    
-#seat_matrix = np.array(input_list[0])
-#for line in input_list[1::]:
-#    seat_matrix = np.vstack([seat_matrix, line])
+
+def calculate_manhattan_distance(n,s,e,w):
+    # calculate manhattan_distance
+    ns = n - s
+    we = e - w
+    if ns < 0:
+        ns = ns * - 1
+    if we < 0:
+        we = we * - 1
+    manhattan_distance = (ns + we)
+    return manhattan_distance
+
 
 def first_star(facing,instruction_list):
     n = 0
     e = 0
     w = 0
     s = 0
-    for step in instruction_list:
-        
+    for step in instruction_list:      
         if step[0] == "N":
             n += int(step[1])
-        
         if step[0] == "E":
             e += int(step[1])
-
         if step[0] == "W":
             w += int(step[1])
-
         if step[0] == "S":
             s += int(step[1])
-
         if step[0] == "F":
             if facing == "north":
                 n += int(step[1])
@@ -47,7 +46,6 @@ def first_star(facing,instruction_list):
                 w += int(step[1])
             if facing == "south":
                 s += int(step[1])
-
         if step[0] == "R":
             rotation = int(step[1]) / 90
             while rotation >= 1:
@@ -60,7 +58,6 @@ def first_star(facing,instruction_list):
                 elif facing == "south":
                     facing = "west"
                 rotation -= 1
-        
         if step[0] == "L":
             rotation = int(step[1]) / 90
             while rotation >= 1:
@@ -73,31 +70,19 @@ def first_star(facing,instruction_list):
                 elif facing == "south":
                     facing = "east"
                 rotation -= 1
-
-    ns = (n - s) * -1
-    we = (e - w) * -1
-    if ns < 0:
-        ns = ns * - 1
-    if we < 0:
-        we = we * - 1
-    manhattan_distance = (ns + we)
-
-    return manhattan_distance
+     # calculate manhattan_distance
+    return calculate_manhattan_distance(n,s,e,w)
 
 def second_star(instruction_list):
-    
     waypoint_n = 1
     waypoint_e = 10
     waypoint_w = 0
     waypoint_s = 0
-
     n = 0
     e = 0
     s = 0
     w = 0
-
     for step in instruction_list:
-
         if step[0] == "N":
             waypoint_n += int(step[1])
         if step[0] == "E":
@@ -106,7 +91,6 @@ def second_star(instruction_list):
             waypoint_w += int(step[1])
         if step[0] == "S":
             waypoint_s += int(step[1])
-            
         if step[0] == "F":
             if waypoint_n > 0:
                 n = n + waypoint_n * int(step[1])
@@ -116,7 +100,6 @@ def second_star(instruction_list):
                 w = w + waypoint_w * int(step[1])
             if waypoint_s > 0:
                 s = s + waypoint_s * int(step[1])
-
         if step[0] == "R":
             dis = int(step[1]) / 90
             while dis >= 1:
@@ -129,7 +112,6 @@ def second_star(instruction_list):
                 waypoint_s = temp_e
                 waypoint_w = temp_s
                 dis -= 1
-
         if step[0] == "L":
             dis = int(step[1]) / 90
             while dis >= 1:
@@ -142,7 +124,6 @@ def second_star(instruction_list):
                 waypoint_s = temp_w
                 waypoint_w = temp_n
                 dis -= 1
-
         waypoint_n = waypoint_n - waypoint_s
         if waypoint_n < 0:
             waypoint_s = waypoint_n * -1
@@ -155,20 +136,13 @@ def second_star(instruction_list):
             waypoint_e = 0
         else:
             waypoint_w = 0
-
-    ns = (n - s) * -1
-    we = (e - w) * -1
-    if ns < 0:
-        ns = ns * - 1
-    if we < 0:
-        we = we * - 1
-    manhattan_distance = (ns + we)
-    return manhattan_distance
+     # calculate manhattan_distance
+    return calculate_manhattan_distance(n,s,e,w)
 
 # first star
 print("First Star")
 print("Answer: {}".format(first_star("east",input_list)))
 
-# second star (too low 45017)
+# second star
 print("Second Star")
 print("Answer: {}".format(second_star(input_list)))
